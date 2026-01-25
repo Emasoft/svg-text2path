c88805e45830ef3d89644c833d62228925ce0ef1
Emasoft <713559+Emasoft@users.noreply.github.com>
2026-01-25 22:58:53 +0100

    release: v0.3.4 - Python 3.11+ requirement update

--- svg_text2path/__init__.py
+++ svg_text2path/__init__.py
@@ -32,7 +32,7 @@ from svg_text2path.tools.dependencies import (
     verify_all_dependencies,
 )
 
-__version__ = "0.3.3"
+__version__ = "0.3.4"
 __author__ = "Emasoft"
 __email__ = "713559+Emasoft@users.noreply.github.com"
 
